2508161f5c3955a1fe6a79c92cd2c51ccff38c90
Sanchit Monga <sanchitmonga22@gmail.com>
2020-06-20 22:31:40 -0400

    Refactored the app to use function based components instead of class components

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,32 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./seasonDisplay";
 import Spinner from "./spinner";
+import useLocation from "./useLocation";
 
-class App extends React.Component {
-    // this is the only time we do a direct assingment to the state
-    // NO DIRECT ASSIGNMENT AFTET THIS POINT
-    state = { lat: null, errorMessage: "" };
-
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({ lat: position.coords.latitude }),
-            (error) => this.state({ errorMessage: error.message })
-        );
+const App = () => {
+    const [lat, errorMessage] = useLocation();
+    let content;
+    if (errorMessage) {
+        content = <div>Error: {errorMessage}</div>;
+    } else if (lat) {
+        content = <SeasonDisplay lat={lat} />;
+    } else {
+        content = <Spinner message="Please accept the location request" />;
     }
 
-    renderContent() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error:{this.state.errorMessage}</div>;
-        }
+    return <div className="border red">{content}</div>;
+};
 
-        if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />;
-        }
-
-        return <Spinner message="Please accept the location request" />;
-    }
-    render() {
-        return <div className="borderRed">{this.renderContent()}</div>;
-    }
-}
 ReactDOM.render(<App />, document.querySelector("#root"));
---
+++ seasons/src/useLocation.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+export default () => {
+    const [lat, setLat] = useState(null);
+    const [errorMessage, setErrorMessage] = useState("");
+    useEffect(() => {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => setLat(position.coords.latitude),
+            (error) => setErrorMessage(error.message)
+        );
+    }, []);
+    return [lat, errorMessage];
+};
